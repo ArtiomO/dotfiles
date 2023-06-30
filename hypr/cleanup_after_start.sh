bb6371daab916d5e38100808a7e63078735df549
Artiom O <ovchinnikov.artiom@gmail.com>
2023-06-30 12:43:53 +0200

    Initial commit

---
+++ hypr/cleanup_after_start.sh
@@ -0,0 +1,6 @@
+sleep 10
+hyprctl keyword windowrule "workspace unset,kitty"
+hyprctl keyword windowrule "workspace unset,google-chrome-stable"
+hyprctl keyword windowrule "workspace unset,code"
+hyprctl keyword windowrule "workspace unset,spotify"
+hyprctl keyword windowrule "workspace unset,telegram-desktop"
